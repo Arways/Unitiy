f34f1bad6d5cb71fd36f3a4debc64bc306f041d9
sdk8263 <sdk8263@naver.com>
2021-12-09 16:30:12 +0900

    1209

---
+++ Assets/Scripts/LoginJoin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginJoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void login()
+    {
+        Debug.Log("login success");
+        SceneManager.LoadScene("MainList");
+    }
+
+    public void goToJoin()
+    {
+        Debug.Log("go to join");
+        SceneManager.LoadScene("Join");
+    }
+
+    public void goToLogin()
+    {
+        Debug.Log("go to login");
+        SceneManager.LoadScene("Login");
+    }
+}
